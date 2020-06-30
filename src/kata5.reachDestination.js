c1d1a5e4dfd3f9e5e1e3e06a0c0174833a18ba01
Tom Hammersley <thomas.j.hammersley@gmail.com>
2020-06-30 19:00:29 +0100

    completed tests & code

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,5 @@
-const reachDestination = (distance, speed) => {};
-
+const reachDestination = (distance, speed) => {
+  const time = ((distance / speed) * 2).toFixed() / 2;
+  return `I should be there in ${time} hours.`;
+};
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,9 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination(44, 10)).toBe("I should be there in 4.5 hours.");
+    expect(reachDestination(25, 4)).toBe("I should be there in 6.5 hours.");
+    expect(reachDestination(80, 25)).toBe("I should be there in 3 hours.");
+  });
 });
